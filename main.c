897e9ff157ab9623a85a7a3ca5f43ae3ea1fd723
@luisfl04_ <luisfelipecontato08@gmail.com>
2024-12-26 22:12:26 -0300

    upd -> Inserção de implementação de arvore binária com funções básicas que manipula essa estrutura

--- main.c
+++ main.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "agenda_telefonica/agenda_telefonica.h"
+#include "arvore/arvore_binaria.h"
 
 int main() {
 
-    Agenda* agenda = criarAgenda();
-    menu(agenda);
-    return 0;
+    ArvoreBinaria* arvore = criarArvoreBinaria();
+
+    inserirElementoNaArvoreBinaria(arvore, 10);
+    inserirElementoNaArvoreBinaria(arvore, 20);
+    inserirElementoNaArvoreBinaria(arvore, 30);
+    inserirElementoNaArvoreBinaria(arvore, 40);
+    inserirElementoNaArvoreBinaria(arvore, 50);
+    inserirElementoNaArvoreBinaria(arvore, 60);
+    inserirElementoNaArvoreBinaria(arvore, 70);
+    inserirElementoNaArvoreBinaria(arvore, 80);
+    inserirElementoNaArvoreBinaria(arvore, 90);
+    inserirElementoNaArvoreBinaria(arvore, 100);    
+
+    imprimirArvoreBinaria(arvore);
 
 }
