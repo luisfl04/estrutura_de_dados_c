63d1f6044e78b840af90a43e0ad2f0388dea0cb1
@luisfl04_ <luisfelipecontato08@gmail.com>
2025-02-20 21:29:25 -0300

    add -> Criação de arquivo 'calculo_informacoes_gerais_arvore_binaria' que conterá funções que calculam valores de altura da árvore e etc..

---
+++ arvore/calculo_informacoes_gerais_arvore_binaria.c
@@ -0,0 +1,32 @@
+/* Arquivo que contém as funções que calculam as informações gerais da árvore binária, em relação a sua altura e etc */
+
+#include <stdio.h>
+#include "arvore_binaria.h"
+
+
+
+int array_de_folhas[100];
+int contagem_de_nos = 0;
+int contagem_de_achagem_de_folhas = 0;
+
+int obterAlturaDeArvore(ArvoreBinaria* arvore_passada){
+    /* Função que retorna a altura de determinada árvore buscando a folha mais distante a partir da raiz */ 
+
+    if(arvore_passada != NULL){
+        contagem_de_nos++;
+        obterAlturaDeArvore(arvore_passada->esquerda);
+        obterAlturaDeArvore(arvore_passada->direita);
+        print("Numero de arestas para achar a folha -> %d", contagem_de_nos);
+        array_de_folhas[contagem_de_achagem_de_folhas] = contagem_de_nos;
+        contagem_de_achagem_de_folhas++;
+        contagem_de_nos = 0;
+    }
+
+}
+
+int obterNumeroDeNosArvore();
+
+int obterProfundidadeDeNo();
+
+
+int obterNumeroDeNosDeAcordoComAltura();
